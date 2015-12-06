3677a7411d7fc9e08e560126c7ac63db08557dde
Jon Goodger <jonno.is@gmail.com>
2015-12-06 09:42:51 +0000

    First 5 days

---
+++ 4.2.rb
@@ -0,0 +1,16 @@
+require 'digest'
+
+def mine_for key
+  digest = Digest::MD5.new
+
+  i = 1
+
+  while true do
+    to_try = key.to_s + i.to_s
+    hash = digest.hexdigest to_try
+    return i if hash.start_with? '000000'
+    i += 1
+  end
+end
+
+puts mine_for 'ckczppom'

bfc1a3750f7a37a2f087b84fb9c5957ee6e6dec5
Jon Goodger <jonno.is@gmail.com>
2015-12-17 21:18:03 +0000

    Test 17 with threading

---
+++ 17.1.thread.rb
@@ -0,0 +1,47 @@
+require 'thread'
+require 'benchmark'
+
+def calculate amount, containers
+  combos = 0
+  mutex = Mutex.new
+  threads = []
+
+  1.upto (containers.size - 12) do |i|
+    threads << Thread.new do
+      containers.combination(i).each do |combo|
+        mutex.synchronize { combos += 1 } if combo.reduce(0, :+) == amount
+      end
+    end
+  end
+
+  threads.map(&:join)
+
+  return combos
+end
+
+containers = [
+  50,
+  44,
+  11,
+  49,
+  42,
+  46,
+  18,
+  32,
+  26,
+  40,
+  21,
+  7,
+  18,
+  43,
+  10,
+  47,
+  36,
+  24,
+  22,
+  40,
+]
+
+puts Benchmark.measure {
+  puts calculate 150, containers
+}

5bfb44b78339ca10735f020eee3feab5df437f20
Jon Goodger <jonno.is@gmail.com>
2015-12-17 23:12:30 +0000

    Add day 17

---
+++ 17.2.rb
@@ -0,0 +1,38 @@
+def calculate amount, containers
+  combos = 0
+
+  1.upto containers.size do |i|
+    containers.combination(i).each do |combo|
+      combos += 1 if combo.reduce(0, :+) == amount
+    end
+
+    break if combos > 0
+  end
+
+  return combos
+end
+
+containers = [
+  50,
+  44,
+  11,
+  49,
+  42,
+  46,
+  18,
+  32,
+  26,
+  40,
+  21,
+  7,
+  18,
+  43,
+  10,
+  47,
+  36,
+  24,
+  22,
+  40,
+]
+
+puts calculate 150, containers
